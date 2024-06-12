f19c018a07336c41ac3b009c7157f2cfac224f78
MinhAnh553 <huynhminhanh553@gmail.com>
2024-06-12 20:06:23 +0700

    Tái cấu trúc

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,17 @@ function changeImage() {
 
     // Mapping between quality and image file name
     const qualityImageMap = {
-        '144p': 'images/144p.png',
-        '240p': 'images/240p.png',
-        '480p': 'images/480p.png',
-        '720p': 'images/720p.png',
-        '1080p': 'images/1080p.png',
+        '144p': './assets/images/144p.png',
+        '240p': './assets/images/240p.png',
+        '480p': './assets/images/480p.png',
+        '720p': './assets/images/720p.png',
+        '1080p': './assets/images/1080p.png',
     };
 
     // Change image source based on selected quality
     qualityImage.src = qualityImageMap[selectedQuality];
 
+    // Phát nhạc
     if (selectedQuality == '1080p') {
         const music = document.getElementById('music');
         music.play();
